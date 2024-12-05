1c7c8bba303388f57fd3edb92541389be10b7373
SeregaUrban <7236187@mail.ru>
2024-12-05 08:25:25 +0300

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,11 @@
+import os
+import time
+directory = os.chdir()
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(file)
+        filetime = os.path.getmtime(file)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        filesize = os.path.getsize(file)
+        parent_dir = os.path.dirname(file)
+
